f48ab15a693397a10e075a23bc45c9d2b7187be6
Abhijit Gupta <avgupta456@gmail.com>
2021-02-27 17:04:23 -0500

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import '@fontsource/roboto';
 
-import LandingContainer from '../LandingContainer';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,9 +18,7 @@ class App extends React.Component {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h2 className="text-4xl sm:text-6xl text-indigo-600">MAIN TITLE</h2>
-            <p className="mt-2 text-2xl sm:text-4xl text-gray-900">
-              Subtitle
-            </p>
+            <p className="mt-2 text-2xl sm:text-4xl text-gray-900">Subtitle</p>
             <p className="max-w-2xl mt-4 mx-auto text-xl text-gray-500">
               {content}
             </p>
